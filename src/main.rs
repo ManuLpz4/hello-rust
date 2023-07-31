5cab97c1fae6362ea8d912a712cca0af701602af
Manu López <manulpz41@gmail.com>
2023-07-30 23:56:36 -0700

    Update ferris-says package

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use std::io::{stdout, BufWriter};
 
 fn main() {
     let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
+    let message = "Hello fellow Rustaceans!";
+    let width = message.len();
 
     let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
+    say(message, width, &mut writer).unwrap();
 }
